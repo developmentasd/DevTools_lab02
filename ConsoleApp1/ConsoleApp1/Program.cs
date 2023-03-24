2ba967198d1f6c2a93d0e98dc9f0d71335e31a34
developmentasd <nastyasha2210@gmail.com>
2023-03-24 12:30:47 +0400

    add new version and console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿PowerCollections.Stack<int> stack = new PowerCollections.Stack<int>();
+
+stack.Push(0);
+stack.Push(1);
+stack.Push(2);
+
+Console.WriteLine(stack.Capacity);
+Console.WriteLine(stack.Count);
+Console.WriteLine(stack.Pop());

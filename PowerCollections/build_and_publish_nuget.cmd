38314060ed88c9f93bb5005207180debd00a821e
developmentasd <nastyasha2210@gmail.com>
2023-03-24 12:19:43 +0400

    add build and publish nuget

---
+++ PowerCollections/build_and_publish_nuget.cmd
@@ -0,0 +1,2 @@
+dotnet pack
+dotnet nuget push "PowerCollections\bin\Debug\*.nupkg" --api-key "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" --source https://nuget.pkg.github.com/developmentasd/index.json --skip-duplicate
